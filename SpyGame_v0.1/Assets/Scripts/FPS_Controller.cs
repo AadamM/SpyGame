081490e149f3b302d936d648c242111f029c27b7
Aadam <armohamm@ucsc.edu>
2017-12-16 12:17:52 -0800

    Added Jumping

--- SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
+++ SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class FPS_Controller : MonoBehaviour {
 
 	public float moveSpeed = 7.5f;
-	public float mouseSens = 3.0f;
+	public float mouseSens = 3f;
 	public float vertClamp = 75f;
+	public float jumpSpeed = 5f;
 
+	float rotH = 0f;
 	float rotV = 0f;
+	float verV = 0f;
+
+	Vector3 speed;
+
+	CharacterController charCon;
 
 	// Use this for initialization
 	void Start (){
 		Screen.lockCursor = true;
+		charCon = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Rotation
-		float rotH = Input.GetAxis("Mouse X") * mouseSens;
+		rotH = Input.GetAxis("Mouse X") * mouseSens;
 		rotV -= Input.GetAxis("Mouse Y") * mouseSens;
 
 		rotV = Mathf.Clamp(rotV, -vertClamp, vertClamp);
@@ -26,14 +36,21 @@ public class FPS_Controller : MonoBehaviour {
 		Camera.main.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
 
 		transform.Rotate(0, rotH, 0);
+
+		if(charCon.isGrounded){
+			if(Input.GetButton("Jump")){
+				verV = jumpSpeed;
+			}
+		} else {
+			verV += Physics.gravity.y * Time.deltaTime;
+		}
 		// Movement
 		float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
 		float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
 
-		Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+		speed = new Vector3(sideSpeed, verV, forwardSpeed);
 		speed = transform.rotation * speed;
-
-		CharacterController charCon = GetComponent<CharacterController>();
-		charCon.SimpleMove(speed);
+		
+		charCon.Move(speed * Time.deltaTime);
 	}
 }
