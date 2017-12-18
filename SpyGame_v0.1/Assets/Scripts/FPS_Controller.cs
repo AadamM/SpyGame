6d296f13f87a76975683259cb28fc149383cb25c
Aadam <armohamm@ucsc.edu>
2017-12-18 13:03:40 -0800

    Added sprinting and crouching

--- SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
+++ SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
@@ -7,16 +7,29 @@ using UnityEngine;
 public class FPS_Controller : MonoBehaviour {
 
 	public float moveSpeed = 7.5f;
+	public float sprintMult = 1.5f;
+	public float crouchSpeedMult = .75f;
+
+	public float crouchHeightMult = .7f;
+
 	public float mouseSens = 3f;
 	public float vertClamp = 75f;
 	public float jumpSpeed = 5f;
-	public float defCamHeight;
 	public float maxCamHeight;
+	public float gravMult = 1.1f;
+
+	float defHeight;
 
 	float rotH = 0f;
 	float rotV = 0f;
+
+	float defSpeed;
 	float verV = 0f;
+	float xSpeed = 0f;
+	float ySpeed = 0f;
+
 	float target;
+	float defCamHeight;
 
 	Vector3 speed;
 	Vector3 camPos;
@@ -24,56 +37,71 @@ public class FPS_Controller : MonoBehaviour {
 	CharacterController charCon;
 	Camera cam;
 
-	// Use this for initialization
 	void Start (){
+		// Making the cursor invisible
 		Screen.lockCursor = true;
+
+		// Collecting default values at runtime
 		charCon = GetComponent<CharacterController>();
 		cam = Camera.main;
-
+		defHeight = charCon.height;
+		defSpeed = moveSpeed;
 		defCamHeight = cam.transform.localPosition.y;
+
+		// Setting target height of cam
 		target = maxCamHeight;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		// Rotation
+		// Horizontal rotation
 		rotH = Input.GetAxis("Mouse X") * mouseSens;
-		rotV -= Input.GetAxis("Mouse Y") * mouseSens;
-
-		rotV = Mathf.Clamp(rotV, -vertClamp, vertClamp);
-
-		cam.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
-
 		transform.Rotate(0, rotH, 0);
 
-		if(charCon.isGrounded){
-			if(Input.GetButton("Jump")){
-				verV = jumpSpeed;
-			}
-		} else {
-			verV += Physics.gravity.y * Time.deltaTime;
-		}
-		// Movement
-		float ySpeed = Input.GetAxis("Vertical") * moveSpeed;
-		float xSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+		// Vertical rotation
+		rotV -= Input.GetAxis("Mouse Y") * mouseSens;
+		rotV = Mathf.Clamp(rotV, -vertClamp, vertClamp);
+		cam.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
 
+		// Configuring viewbobbing
 		camPos = cam.transform.localPosition;
 		if(( (ySpeed!=0) || (xSpeed!=0) ) && charCon.isGrounded){
-			//Debug.Log(target.ToString() + camPos.y.ToString());
-
 			if(Mathf.Abs(Mathf.Abs(camPos.y)-Mathf.Abs(defCamHeight+target)) < maxCamHeight/5){
 				target=-target;
 			}
 
-			camPos.y = Mathf.Lerp(camPos.y, defCamHeight+target, .1f);
+			camPos.y = Mathf.Lerp(camPos.y, defCamHeight+target, ((moveSpeed/(defSpeed*sprintMult))*.17f));
 		} else {
 			camPos.y = Mathf.Lerp(camPos.y, defCamHeight, .1f);
 		}
 		cam.transform.localPosition = camPos;
 
+		// Jumping
+		if(charCon.isGrounded){
+			if(Input.GetButton("Jump")){
+				verV = jumpSpeed;
+			}
+		} else {
+			verV += Physics.gravity.y * gravMult * Time.deltaTime;
+		}
+
+		// Collecting input for sprinting and crouching
+		if(Input.GetKey(KeyCode.LeftShift)){
+			moveSpeed = defSpeed * sprintMult;
+			charCon.height = defHeight;
+		} else if(Input.GetKey(KeyCode.C)){
+			moveSpeed = defSpeed * crouchSpeedMult;
+			charCon.height = defHeight * crouchHeightMult;
+		} else {
+			moveSpeed = defSpeed;
+			charCon.height = defHeight;
+		}
+		// Collecting movment input
+		ySpeed = Input.GetAxis("Vertical") * moveSpeed;
+		xSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+
+		// Translating movment input to actual movement
 		speed = new Vector3(xSpeed, verV, ySpeed);
 		speed = transform.rotation * speed;
-		
 		charCon.Move(speed * Time.deltaTime);
 	}
 }
