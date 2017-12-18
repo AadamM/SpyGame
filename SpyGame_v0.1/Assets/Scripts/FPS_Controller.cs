15f8628f0f37dbe38d9bdecd182f6d81c6bd1b8c
Aadam <armohamm@ucsc.edu>
2017-12-18 12:25:50 -0800

    Added ViewBobbing

--- SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
+++ SpyGame_v0.1/Assets/Scripts/FPS_Controller.cs
@@ -10,19 +10,28 @@ public class FPS_Controller : MonoBehaviour {
 	public float mouseSens = 3f;
 	public float vertClamp = 75f;
 	public float jumpSpeed = 5f;
+	public float defCamHeight;
+	public float maxCamHeight;
 
 	float rotH = 0f;
 	float rotV = 0f;
 	float verV = 0f;
+	float target;
 
 	Vector3 speed;
+	Vector3 camPos;
 
 	CharacterController charCon;
+	Camera cam;
 
 	// Use this for initialization
 	void Start (){
 		Screen.lockCursor = true;
 		charCon = GetComponent<CharacterController>();
+		cam = Camera.main;
+
+		defCamHeight = cam.transform.localPosition.y;
+		target = maxCamHeight;
 	}
 	
 	// Update is called once per frame
@@ -33,7 +42,7 @@ public class FPS_Controller : MonoBehaviour {
 
 		rotV = Mathf.Clamp(rotV, -vertClamp, vertClamp);
 
-		Camera.main.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
+		cam.transform.localRotation = Quaternion.Euler(rotV, 0, 0);
 
 		transform.Rotate(0, rotH, 0);
 
@@ -45,10 +54,24 @@ public class FPS_Controller : MonoBehaviour {
 			verV += Physics.gravity.y * Time.deltaTime;
 		}
 		// Movement
-		float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
-		float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+		float ySpeed = Input.GetAxis("Vertical") * moveSpeed;
+		float xSpeed = Input.GetAxis("Horizontal") * moveSpeed;
 
-		speed = new Vector3(sideSpeed, verV, forwardSpeed);
+		camPos = cam.transform.localPosition;
+		if(( (ySpeed!=0) || (xSpeed!=0) ) && charCon.isGrounded){
+			//Debug.Log(target.ToString() + camPos.y.ToString());
+
+			if(Mathf.Abs(Mathf.Abs(camPos.y)-Mathf.Abs(defCamHeight+target)) < maxCamHeight/5){
+				target=-target;
+			}
+
+			camPos.y = Mathf.Lerp(camPos.y, defCamHeight+target, .1f);
+		} else {
+			camPos.y = Mathf.Lerp(camPos.y, defCamHeight, .1f);
+		}
+		cam.transform.localPosition = camPos;
+
+		speed = new Vector3(xSpeed, verV, ySpeed);
 		speed = transform.rotation * speed;
 		
 		charCon.Move(speed * Time.deltaTime);
